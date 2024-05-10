578f007bce33f99c91689fd291d877c739894447
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-05-09 22:39:11 -0400

    Update gameboard class and tests
    -Add tests for placeShip and recieveAttack function.
    -Update gameboard class, add placeShip function that puts ship on grid from given coords.
    -Add test for error for placeShip function.

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -1,3 +1,5 @@
+const Ship = require('./ship')
+
 class Gameboard {
     constructor(rows, cols) {
         this.rows = rows;
@@ -13,8 +15,36 @@ class Gameboard {
         }
     }
 
-    placeShip() {
+    placeShip(startY, startX, length, orientation) {
+        // Create new ship object
+        const ship = new Ship(length);
 
+        // Placing ship on coordinates
+        if (orientation === "horizontal") {
+            for (let i = 0; i < length; i++) {
+                if (startX + i >= this.cols) {
+                    throw new Error("Ship placement out of bounds")
+                }
+
+                if (this.grid[startY][startX + i] !== null) {
+                    throw new Error("Cannot place ship here");
+                }
+
+                this.grid[startY][startX + i] = ship;
+            }
+        } else if (orientation === "vertical") {
+            for (let i = 0; i < length; i++) {
+                if (startY + i >= this.rows) {
+                    throw new Error("Ship placement out of bounds")
+                }
+
+                if (this, grid[startY + i][startX] !== null) {
+                    throw new Error("Cannot place ship here");
+                }
+
+                this.grid[startY + i][startX] = ship;
+            }
+        }
     }
 }
 
--- scripts/gameboard.test.js
+++ scripts/gameboard.test.js
@@ -1,4 +1,5 @@
 const Gameboard = require('./gameboard');
+const Ship = require('./ship')
 
 describe('Gameboards', () => {
     describe('should initialize a grid based on declared rows and cols when', () => {
@@ -13,11 +14,25 @@ describe('Gameboards', () => {
     })
 
     describe('placeShip function', () => {
-        it.todo('should be able to place ships at specific coordinates by calling the ship class')
+        it('should be able to place ships at specific coordinates by calling the ship class', () => {
+            let newBoard = new Gameboard(5, 5);
+            newBoard.placeShip(1, 2, 2, "horizontal")
+            const cell = newBoard.grid[1][2];
+            expect(cell).toBeInstanceOf(Ship);
+        });
+
+        it('should return an error if out of bounds', () => {
+            let newBoard = new Gameboard(4, 4);
+            expect(() => newBoard.placeShip(-1, -1, 3, 'vertical')).toThrowError()
+        });
     })
 
+    describe("recieveAttack function", () => {
+        it('should takes a pair of coordinates and determines if the attack hit a ship.', () => {
+            let newBoard = new Gameboard(8, 8);
+            newBoard.placeShip(0, 0, 5, 'vertical');
 
-    it.todo('should have a recieveAttack function that takes a pair of coordinates and determines if the attack hit a ship.')
-
-
+            expect(newBoard.recieveAttack(0, 0)).toBe(true)
+        })
+    })
 })
