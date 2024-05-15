8415328a7c58a5e07f43dcb2b314ecaef364f81b
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-05-15 15:42:01 -0400

    Change webpack.config and add style-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // import Ship from './ship';
 // import Gameboard from './gameboard';
 // import Player from './player';
+
+import './style.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,41 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
-
-
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-
-
-
-const config = {
-    entry: './src/index.js',
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, './src/index.js')
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        assetModuleFilename: 'images/[hash][ext][query]'
     },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
             },
             {
                 test: /\.css$/i,
-                use: [stylesHandler, 'css-loader', 'postcss-loader'],
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
             },
             {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        config.plugins.push(new MiniCssExtractPlugin());
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battleship Game',
+            filename: 'index.html',
+            template: './src/template.html'
+        }),
+    ]
 };
